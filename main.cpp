8453f34c41fcd08dd7c0b3f2401ad3b38c76778e
cmcdev-code <collinhalo5@outlook.com>
2023-03-11 23:28:53 -0500

    Setup SFML

--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
-#include <iostream>
-#include "logic_for_particles.hpp"
+#include <SFML/Graphics.hpp>
 
+int main()
+{
+   
 
-int main() {
-
-	vec3<double> positionLocal(3,4,5);
-	vec3<double> velocityLocal(3, 4, 5);
-	vec3<double> accelerationLocal(3, 4, 5);
-	
-	particle<double> test(positionLocal, velocityLocal, accelerationLocal, 100, 0, 100);
-
-
-	std::cout << "Test : " << test.get_x_position()<<" "<<test.get_i_position()<<" "<<test.get_z_position()<<"\n";
-
-	logic_particles::update_particle_position(test);
-	std::cout << "Test : " << test.get_x_position() << " " << test.get_i_position() << " " << test.get_z_position() << "\n";
-
-
-
-
-
-
-	return 0;
+    return 0;
 }
